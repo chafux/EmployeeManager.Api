f1a5b1d065590db4844146e6fbb3ffc0c6b97fa2
Mohamed Amine Chafia <chafia.amine1@gmail.com>
2023-11-22 02:00:33 -0500

    The interfaces IEmployeeRepository and ICountryRepository have been added

---
+++ EmployeeManager.Api/Repositories/EmployeeSqlRepository.cs
@@ -0,0 +1,45 @@
+﻿using EmployeeManager.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Api.Repositories
+{
+    public class EmployeeSqlRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext db = null;
+
+        public EmployeeSqlRepository(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Employee> SelectAll()
+        {
+            List<Employee> data = db.Employees.FromSqlRaw("SELECT EmployeeID, FirstName, LastName, Title, BirthDate, HireDate, County, Notes FROM Employees ORDER BY EmployeeID ASC").ToList();
+            return data;
+        }
+
+        public Employee SelectByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

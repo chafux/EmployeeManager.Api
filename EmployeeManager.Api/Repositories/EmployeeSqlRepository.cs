e37e5f0370134b748191536731aa6c3e8a5c2a46
Mohamed Amine Chafia <chafia.amine1@gmail.com>
2023-11-23 01:39:23 -0500

    EmployeeSqlRepository and CountrySqlRepository have been implemented

--- EmployeeManager.Api/Repositories/EmployeeSqlRepository.cs
+++ EmployeeManager.Api/Repositories/EmployeeSqlRepository.cs
@@ -1,9 +1,10 @@
-﻿using EmployeeManager.Api.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManager.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace EmployeeManager.Api.Repositories
 {
@@ -18,28 +19,31 @@ namespace EmployeeManager.Api.Repositories
 
         public List<Employee> SelectAll()
         {
-            List<Employee> data = db.Employees.FromSqlRaw("SELECT EmployeeID, FirstName, LastName, Title, BirthDate, HireDate, County, Notes FROM Employees ORDER BY EmployeeID ASC").ToList();
+            List<Employee> data = db.Employees.FromSqlRaw("SELECT EmployeeID, FirstName, LastName, Title, BirthDate, HireDate, Country, Notes FROM Employees ORDER BY EmployeeID ASC").ToList();
+
             return data;
         }
 
         public Employee SelectByID(int id)
         {
-            throw new NotImplementedException();
+            Employee emp = db.Employees.FromSqlRaw("SELECT EmployeeID, FirstName, LastName, Title, BirthDate, HireDate, Country, Notes FROM Employees WHERE EmployeeID={0}", id).SingleOrDefault();
+
+            return emp;
         }
 
         public void Insert(Employee emp)
         {
-            throw new NotImplementedException();
+            int count = db.Database.ExecuteSqlRaw("INSERT INTO Employees(FirstName, LastName, Title, BirthDate, HireDate, Country, Notes)  VALUES({0},{1},{2},{3},{4},{5},{6})", emp.FirstName, emp.LastName, emp.Title, emp.BirthDate, emp.HireDate, emp.Country, emp.Notes);
         }
 
         public void Update(Employee emp)
         {
-            throw new NotImplementedException();
+            int count = db.Database.ExecuteSqlRaw("UPDATE Employees SET FirstName={0}, LastName={1}, Title={2}, BirthDate={3}, HireDate={4}, Country={5}, Notes={6} WHERE EmployeeID={7}", emp.FirstName, emp.LastName, emp.Title, emp.BirthDate, emp.HireDate, emp.Country, emp.Notes, emp.EmployeeID);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            int count = db.Database.ExecuteSqlRaw("DELETE FROM Employees WHERE EmployeeID={0}", id);
         }
     }
 }

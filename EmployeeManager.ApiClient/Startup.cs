3788e6bd090ec3eb6ec412d6732e3e884c80b04d
Mohamed Amine Chafia <chafia.amine1@gmail.com>
2023-12-07 06:43:55 -0500

    Setting Up the Httpclient

--- EmployeeManager.ApiClient/Startup.cs
+++ EmployeeManager.ApiClient/Startup.cs
@@ -1,24 +1,65 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using EmployeeManager.ApiClient.Models;
+using EmployeeManager.ApiClient.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Hosting;
+
+
 
 namespace EmployeeManager.ApiClient
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+
+        private IConfiguration config = null;
+
+
+        public Startup(IConfiguration config)
         {
+            this.config = config;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+        options.UseSqlServer(this.config.GetConnectionString("AppDb")));
+
+            services.AddIdentity<AppIdentityUser, AppIdentityRole>()
+                    .AddEntityFrameworkStores<AppIdentityDbContext>();
+
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.LoginPath = "/Security/SignIn";
+                opt.AccessDeniedPath = "/Security/AccessDenied";
+            });
+
+
+            HttpClient client = new HttpClient();
+            string baseUrl = config.GetValue<string>("AppSettings:BaseUrl");
+            client.BaseAddress = new Uri(baseUrl);
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+
+            services.AddSingleton<HttpClient>(client);
+
+            //services.Configure<WebApiConfig>(config.GetSection("AppSettings"));
+
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -26,15 +67,19 @@ namespace EmployeeManager.ApiClient
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=EmployeeManager}/{action=List}/{id?}");
             });
+
         }
     }
 }
